56c2e8952fd42a0792ad6d53c87f52e82f417bae
zheka <zheka0783@gmail.com>
2016-08-12 15:57:42 +0300

    Added output of list of users for admin

--- spring-security/src/main/java/com/devblogs/web/controller/SecurityController.java
+++ spring-security/src/main/java/com/devblogs/web/controller/SecurityController.java
@@ -1,5 +1,7 @@
 package com.devblogs.web.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,14 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import com.devblogs.model.User;
+import com.devblogs.service.UserService;
 
 @Controller
 public class SecurityController {
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
 	public ModelAndView adminPage() {
+		List<User> users = userService.findAll();
 		ModelAndView model = new ModelAndView();
 		model.addObject("message", "This page is for ROLE_ADMIN only!");
+		model.addObject("users", users);
 		model.setViewName("admin");
 		return model;
 	}
